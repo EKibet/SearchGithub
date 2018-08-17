1d86622a922174160a88d69b978c9cc370ac7d42
EKibet <kibetedgar@gmail.com>
2018-08-17 16:50:05 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
