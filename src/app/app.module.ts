00d65f6bc43e9c735495c99032d2082f01df8af2
EKibet <kibetedgar@gmail.com>
2018-08-22 07:26:45 +0300

    updates app.module provider and imports properties

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ProfileService } from './profile.service';
+import {TimeAgoPipe} from 'time-ago-pipe';
+import {NgxPaginationModule} from 'ngx-pagination'; // <-- import the module
 
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { Http, Headers } from '@angular/http';
-import 'rxjs/add/operator/map';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { UserDirective } from './user.directive';
+import { UserComponent } from './user/user.component';
+import {RoutingModule} from './routing/routing.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    LandingPageComponent,
+    UserDirective,
+    UserComponent,
+    TimeAgoPipe,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RoutingModule,
+    NgxPaginationModule
   ],
-  providers: [],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
